11059ab90b24f5ba4195617c04682c540f35bea3
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-04-28 12:16:58 -0500

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="text-center bg-gray-800 text-white p-4 mb-10">
+    <div class="text-white font-bold text-3xl">
+      <i class="fas fa-exclamation-circle"></i> Covid-19 Alert News
+    </div>
+    <p class="text-blue-500 font-bold">
+      Powered By
+      <a class="text-xs" href="covid-19alert.info">covid-19alert.info</a>
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+</style>'
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 class="text-center font-bold text-blue-150">
+      <i class="fas fa-rss-square"></i> Recent News
+      <i class="fas fa-angle-double-down"></i>
+    </h1>
+    <h2 class="text-center text-red-100 font-bold mb-4">
+      <i class="fab fa-youtube"></i> Most Recent News From Youtube
+    </h2>
+    <div class="align-center">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/qJ3jnGCtPUs?controls=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
